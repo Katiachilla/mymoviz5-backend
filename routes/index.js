26da1d2e195101e39737925e59f994b46b6c8a55
katia <ghilasmaouche89@gmail.com>
2023-07-05 15:18:48 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var router = express.Router();
+var fetch = require('node-fetch')
+const api_key = process.env.API_KEY ;
+console.log(api_key);
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+    .then(res=>res.json())
+    .then(data=>{res.json({ movies: [data ]})});
+  
+    
+ });
+
+
+
+module.exports = router;
+
