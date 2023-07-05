3fc1425de95f0db0b5e9742065653684be309eda
katia <ghilasmaouche89@gmail.com>
2023-07-05 15:36:11 +0200

    back

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ console.log(api_key);
 router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
     .then(res=>res.json())
-    .then(data=>{res.json({ movies: [data ]})});
+    .then(data=>{res.json({ movies: data.results })});
   
     
  });
